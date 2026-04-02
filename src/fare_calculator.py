ea020b7cedad3b4e24f74465dbe764e2e139d14c
harshil1424 <harshilkanojiya983@gmail.com>
2026-04-02 14:07:20 +0400

    Refactor fare calculation and display functions

--- src/fare_calculator.py
+++ src/fare_calculator.py
@@ -1,14 +1,30 @@
-def calculate_fare(origin, destination):
-    base_fare = 2
-    zone_difference = abs(origin - destination)
-
-    fare = base_fare + zone_difference
-
-    return fare
+def calculate_zones(origin_zone, destination_zone):
+    zones = abs(origin_zone - destination_zone) + 1
+    return zones
 
 
-def display_fare(origin, destination, fare):
-    print("\n----- Ticket Details -----")
-    print("Origin Zone:", origin)
-    print("Destination Zone:", destination)
-    print("Ticket Fare: $", fare)
+def calculate_fare(origin_zone, destination_zone, passenger_type):
+
+    price_per_zone = 2
+
+    zones = calculate_zones(origin_zone, destination_zone)
+
+    fare = zones * price_per_zone
+
+    if passenger_type == "Student":
+        fare = fare * 0.5
+
+    elif passenger_type == "Senior":
+        fare = fare * 0.7
+
+    return round(fare, 2)
+
+
+def display_ticket(origin, destination, zones, passenger_type, fare):
+
+    print("\n------ CTA Underground Ticket ------")
+    print("From:", origin)
+    print("To:", destination)
+    print("Zones Travelled:", zones)
+    print("Passenger Type:", passenger_type)
+    print("Ticket Price: $", fare)
