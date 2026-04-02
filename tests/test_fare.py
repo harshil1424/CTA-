ac1ce6d7758b0309ef8cb0725b5fd404a721cc32
harshil1424 <harshilkanojiya983@gmail.com>
2026-04-02 13:10:43 +0400

    Add tests for calculate_fare function

--- tests/test_fare.py
+++ tests/test_fare.py
@@ -1,1 +1,7 @@
+from src.fare_calculator import calculate_fare
 
+
+def test_calculate_fare():
+    assert calculate_fare(1, 3) == 4
+    assert calculate_fare(2, 2) == 2
+    assert calculate_fare(3, 5) == 4
