5dae8b5a19d4ff4f7fd3bb668789109f67a71a4b
harshil1424 <harshilkanojiya983@gmail.com>
2026-04-02 14:07:48 +0400

    Update get_zone_input to get station names
    Refactor input functions to use station names instead of zone numbers.

--- src/validators.py
+++ src/validators.py
@@ -1,12 +1,49 @@
-def get_zone_input(message):
+stations = {
+    "Centrala": 1,
+    "Jaund": 1,
+    "Soth": 1,
+    "Norvia": 2,
+    "Eston": 2,
+    "Zord": 3,
+    "Velka": 3
+}
+
+
+def get_station(message):
+
     while True:
-        try:
-            zone = int(input(message))
 
-            if zone < 1 or zone > 5:
-                print("Please enter a zone between 1 and 5.")
-            else:
-                return zone
+        print("\nAvailable Stations:")
+        for station in stations:
+            print("-", station)
 
-        except ValueError:
-            print("Invalid input. Please enter a number.")
+        name = input(message)
+
+        if name in stations:
+            return name
+        else:
+            print("Station not found. Please enter a valid station name.")
+
+
+def get_passenger_type():
+
+    print("\nPassenger Type")
+    print("1. Adult")
+    print("2. Student")
+    print("3. Senior")
+
+    while True:
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            return "Adult"
+
+        elif choice == "2":
+            return "Student"
+
+        elif choice == "3":
+            return "Senior"
+
+        else:
+            print("Invalid choice")
